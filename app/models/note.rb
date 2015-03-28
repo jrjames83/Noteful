c2a8891da1042023aed7124f856c4421c35a05a3
Jeffrey James <jeffjames.vib@gmail.com>
2015-03-28 17:38:48 +0000

    These are a bunch of changes

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,5 +5,13 @@ class Note < ActiveRecord::Base
   validates :content, presence: true
 
   scope :sorted, lambda { order('created_at DESC') }
-
+  
+# def preview
+ #   @note = "#{Note.find(params[:id]).(0...99)}..."
+ #end
+  
+  def preview #get the content of the note being used or looked up
+  self.content.slice(0..100) << "...(readmore)"
+  end
+  
 end
